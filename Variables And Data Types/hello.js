8c8030163656efa54e56aa83ad29c4cc03a1355f
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-06-07 22:18:05 +0530

    JavaScript

---
+++ Variables And Data Types/hello.js
@@ -0,0 +1,18 @@
+/* Variable is kind of any word Jisme tum value ko store kar rahe ho*/
+// fullName = "Lokesh";
+// age = 21;
+// rollNo = 246;
+
+X = null; // output is : null
+Y = undefined; // outPut is : undefined
+
+console.log(X, Y);
+// console.log("Name is :",fullName,"| Age is :", age, "| Roll No. is :", rollNo);
+
+
+
+let name = "lokesh";
+var age = 21;
+const value = 123;
+
+console.log(name, age, value);
